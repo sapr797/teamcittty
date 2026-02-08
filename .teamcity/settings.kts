26b9cc3e43f52f753c3ba1395726bd6de05e72ce
admin <admin@TeamCity>
2026-02-08 14:21:02 +0000

    runners of 'Build 2' build configuration were updated (TeamCity change in 'Teamcittty' project)

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,6 +1,7 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
 import jetbrains.buildServer.configs.kotlin.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
 
 /*
@@ -141,6 +142,15 @@ object Build2 : BuildType({
             coverageEngine = jacoco {
                 classLocations = "target/classes"
             }
+        }
+        script {
+            name = "Hunter"
+            id = "Hunter"
+            scriptContent = """
+                mvn clean compile
+                mvn test
+                mvn exec:java -Dexec.mainClass="plaindoll.HunterDemo"
+            """.trimIndent()
         }
     }
 

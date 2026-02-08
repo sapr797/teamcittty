230c422fa72d9d0fa2b712d031d481cf6328fa1c
sapr797 <unisapr797@gmail.com>
2026-02-08 18:20:51 +0300

    Update Welcomer.java

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -13,4 +13,7 @@ public class Welcomer{
 	public String saySome(){
 		return "something in the way";
 	}
+	 public String getHunterReplica() {
+    return "A hunter is a hunter, even in a dream.";
+  }
 }
